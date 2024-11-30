c72ab14922a53c9b88ed6f48f4716fa9711b420c
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-12-01 03:00:22 +0530

    [FIX] Added a fix for empty proofs

--- backend/src/routes/reclaim.ts
+++ backend/src/routes/reclaim.ts
@@ -1,7 +1,9 @@
-import { Request, Response, Router } from "express";
+import express, { Request, Response, Router } from "express";
 import { generateConfig, recieveProofs } from "../controllers/reclaim";
 
 export const reclaimRouter = Router()
+
+reclaimRouter.use(express.text({ type: '*/*', limit: '50mb' }));
 
 reclaimRouter.get('/generate-config', generateConfig)
 reclaimRouter.post('/recieve-proofs', recieveProofs)
